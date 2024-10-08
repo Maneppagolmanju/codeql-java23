86aebc7993c6c2e8757c3e3ab3fdf4e1ece2e4c1
Danish Nawab <danish.nawab@sixt.com>
2024-10-08 10:08:39 +0200

    set up basic project structure

---
+++ src/main/java/com/example/codeql_demo/CodeqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeql_demo/CodeqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
